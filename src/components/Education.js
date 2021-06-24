cda464ada3f23f53e9486020409938a4fdcb135f
Albert Felix <59144143+albert-felix@users.noreply.github.com>
2021-06-24 17:33:01 +0530

    added education info

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
 
 const Education = () => {
-  return(
+  return (
     <div>
-      Education
+      <h3>EDUCATION</h3>
+      <div className="education">
+        <h6>Master of Planning</h6>
+        <p className="edu-ins">School of Architecture and Planning, Anna University</p>
+        <div className="edu-year">
+        <p>2016 - 2018</p>
+        <p>Chennai</p>
+        </div>
+        <p className="cgpa">CGPA - 7.0</p>
+      </div>
+      <div className="education">
+        <h6>Bachelor of Engineering (B.E - Civil)</h6>
+        <p className="edu-ins">Government College of Engineering</p>
+        <div className="edu-year">
+        <p>2012 - 2016</p>
+        <p>Salem</p>
+        </div>
+        <p className="cgpa">CGPA - 8.53</p>
+      </div>
+      <div className="education">
+        <h6>HSC - 94 %</h6>
+        <p className="edu-ins">School of Architecture and Planning, Anna University</p>
+        <div className="edu-year">
+        <p>2016 - 2018</p>
+        <p>Chennai</p>
+        </div>
+        <p className="cgpa">CGPA - 7.0</p>
+      </div>
+      
+      <div className="education">
+        <h6>SSLC - 93.6 %</h6>
+        <p className="edu-ins">School of Architecture and Planning, Anna University</p>
+        <div className="edu-year">
+        <p>2016 - 2018</p>
+        <p>Chennai</p>
+        </div>
+        <p className="cgpa">CGPA - 7.5</p>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Education;
